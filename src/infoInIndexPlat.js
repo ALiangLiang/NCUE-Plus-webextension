f35954793e39e398bba4e4d82474fc3bd7447265
ALiangLiang <david27072183@gmail.com>
2016-06-06 12:31:10 +0800

    Fix Ad

--- src/infoInIndexPlat.js
+++ src/infoInIndexPlat.js
@@ -10,9 +10,6 @@ const infoModal = {
 					<p>
 						您正在使用由學生會-資訊組提供的
 						<br>「${"NCUE教務系統 Supporter (非官方)".link("https://chrome.google.com/webstore/detail/agbcepaalgmkkbfognoaonhippllckkc")}」擴充功能
-						<br>
-						歡迎至網站參觀或向同學朋友推薦:
-						${"學生會粉絲專頁".link("https://www.facebook.com/國立彰化師範大學-學生會-128794183874722/")}
 					</p>
 				</div>
 	    </td>

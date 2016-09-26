8668d722b4e8e911e2876c1507ba5ca6db196636
ALiangLiang <david27072183@gmail.com>
2016-09-26 16:43:46 +0800

    Release 1.6.0 新增 網路加退選的驗證碼辨識

--- src/infoInIndexPlat.js
+++ src/infoInIndexPlat.js
@@ -1,14 +1,14 @@
 const infoModal = {
-	create : function (parent) {
-		var node = document.createElement("table");
-		node.innerHTML = this.modal;
-		parent.insertBefore(node.querySelector("tbody > tr"), parent.querySelector("tr:nth-child(2)"));
-	},
-	modal : `<tr>
+  create: function(parent) {
+    var node = document.createElement("table");
+    node.innerHTML = this.modal;
+    parent.insertBefore(node.querySelector("tbody > tr"), parent.querySelector("tr:nth-child(2)"));
+  },
+  modal: `<tr>
 	    <td colspan="2">
 				<div class="alert alert-info" style="margin-bottom:0px">
 					<p>
-						您正在使用由學生會-資訊組提供的
+						您正在使用由${'學生會'.link('https://www.facebook.com/128794183874722/')}-資訊部提供的
 						<br>「${"NCUE教務系統 Supporter (非官方)".link("https://chrome.google.com/webstore/detail/agbcepaalgmkkbfognoaonhippllckkc")}」擴充功能
 					</p>
 				</div>
